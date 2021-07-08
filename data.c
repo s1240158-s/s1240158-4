6d945a0a9d00696d027fac8971ebb0bf21386bf5
s1240158 <s1240158@ilab1dc6.u-aizu.ac.jp>
2021-07-08 22:51:45 +0900

    Initial revision

---
+++ data.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+  int a,b;
+  char str[10];
+  /*printf("What is your name?\n");
+  scanf("%s",str);
+  printf("Hello, %s\n",str);*/
+  srand((unsigned int)time(NULL));
+  a=rand()%6+1;
+  b=rand()%6+1;
+  printf("Rolling the dice...\n");
+  printf("Die 1: %d\n",a);
+  printf("Die 2: %d\n",b);
+  printf("Total value: %d\n", a+b);
+  /*if(a+b>7) printf("%s Won\n",str);
+    else printf("%s Lost\n",str);*/
+  
+
+  return 0;
+}
