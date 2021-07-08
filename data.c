c924fd8156a547a69cec35e9ecce2d6a2a593e49
s1240158 <s1240158@ilab1dc6.u-aizu.ac.jp>
2021-07-08 22:57:31 +0900

    Won/lost message added

--- data.c
+++ data.c
@@ -5,9 +5,9 @@
 int main(){
   int a,b;
   char str[10];
-  /*printf("What is your name?\n");
+  printf("What is your name?\n");
   scanf("%s",str);
-  printf("Hello, %s\n",str);*/
+  printf("Hello, %s\n",str);
   srand((unsigned int)time(NULL));
   a=rand()%6+1;
   b=rand()%6+1;
@@ -15,8 +15,8 @@ int main(){
   printf("Die 1: %d\n",a);
   printf("Die 2: %d\n",b);
   printf("Total value: %d\n", a+b);
-  /*if(a+b>7) printf("%s Won\n",str);
-    else printf("%s Lost\n",str);*/
+  if(a+b>7) printf("won\n");
+  else printf("lost\n");
   
 
   return 0;
